b40994b0d881e609bea6e06a6f797222350823eb
jinha_mac <jinha12345@naver.com>
2024-01-26 15:05:25 +0900

    이미지 다운로드 개선 전 최종 정리

--- app.py
+++ app.py
@@ -392,4 +392,5 @@ if __name__ == '__main__':
 
     webbrowser.open('http://127.0.0.1:5000/')
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
+    #dddd
     
